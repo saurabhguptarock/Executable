534ec4e1d5d27ac867909274d8ea7a1d8e2b4abe
saurabh gupta <32234038+saurabhguptarock@users.noreply.github.com>
2018-07-11 09:34:08 +0530

    Add files via upload

---
+++ Make_Executable.py
@@ -0,0 +1,4 @@
+from cx_Freeze import setup, Executable
+#run from command window as python Make_Executable.py build
+
+setup(name='jarvis', version='1', description='jarvis', executables=[Executable('Jarvis_Mark1.py')])
